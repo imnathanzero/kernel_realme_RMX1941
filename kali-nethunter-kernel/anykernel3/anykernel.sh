1f4708814a417f566d4f389f4d3a3744ed2685c3
Frostleaft07 <zx7unknow@gmail.com>
2024-09-16 00:19:58 +0000

    import kali builder from gitlab

---
+++ kali-nethunter-kernel/anykernel3/anykernel.sh
@@ -0,0 +1,52 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+# Changes for SkyDragon by HolyAngel @ xda-developers
+# Changes for NetHunter by Re4son
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=NetHunter Kernel
+do.devicecheck=1
+do.modules=1
+do.systemless=0 #Never use this for NetHunter kernels as it prevents us from writing to /lib/modules
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=RMX1941
+device.name2=RMX1945
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## Trim partitions
+fstrim -v /cache;
+fstrim -v /data;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# init.rc
+# backup_file init.rc;
+# replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+
+
+# end ramdisk changes
+
+write_boot;
+## end install
